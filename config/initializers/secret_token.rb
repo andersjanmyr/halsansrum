11037156464e693b6fb12f802f809e18a5b374ec
Anders Janmyr <anders@janmyr.com>
2011-08-06 13:14:28 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Halsansrum::Application.config.secret_token = '51479b3f76dde122520f776e3e95f29c568705b4895f46f3c0e039e7c5ad4482968532475cd38df5c4fe8113fc4f1f857bd426f9256c9d8899cbef224bdfe6bd'
